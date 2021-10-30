82e3207034f70f223ae8d6166fdbc66f944a50b8
Adam Evans <Zankomag@gmail.com>
2021-10-31 01:30:39 +0300

    Fix comment

--- src/NpuSchedule.Core/Models/ClassInfo.cs
+++ src/NpuSchedule.Core/Models/ClassInfo.cs
@@ -1,6 +1,6 @@
 namespace NpuSchedule.Core.Models
 {
-    /// <summary>University class (the time when students get together to listen to a lesson of a particular subject in school)</summary>
+    /// <summary>University class info (the time when students get together to listen to a lesson of a particular subject in school)</summary>
     public class ClassInfo
     {
         public string Name { get; internal set; }
