0917b4f304b5d5c3344c8ea1b91b07de4b018105
Adam Evans <Zankomag@gmail.com>
2021-10-31 01:37:59 +0300

    Improve fields naming

--- src/NpuSchedule.Core/Models/ClassInfo.cs
+++ src/NpuSchedule.Core/Models/ClassInfo.cs
@@ -3,9 +3,9 @@ namespace NpuSchedule.Core.Models
     /// <summary>University class info (the time when students get together to listen to a lesson of a particular subject in school)</summary>
     public class ClassInfo
     {
-        public string Name { get; internal set; }
+        public string DisciplineName { get; internal set; }
         public string Teacher { get; internal set; }
         public string Classroom { get; internal set; }
-        public string UrlLink { get; internal set; }
+        public string OnlineMeetingUrl { get; internal set; }
     }
 }
