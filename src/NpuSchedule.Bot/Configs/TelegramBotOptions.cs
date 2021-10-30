0adae2119fcd4dd409fa2c800971c797406a9ea7
Adam Evans <Zankomag@gmail.com>
2021-10-30 20:32:49 +0300

    Fix Telegram bot config

--- src/NpuSchedule.Bot/Configs/TelegramBotOptions.cs
+++ src/NpuSchedule.Bot/Configs/TelegramBotOptions.cs
@@ -3,6 +3,7 @@
 
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using NpuSchedule.Common.Attributes;
 
 namespace NpuSchedule.Bot.Configs {
 
@@ -14,15 +15,7 @@ namespace NpuSchedule.Bot.Configs {
 		[RegularExpression(@"^\S*$", ErrorMessage = "No white space allowed")]
 		public string Token { get; init;  }
 		public long[] AdminIds { get; init;  }
-
-		[Required]
-		[RegularExpression(@"^\S*$", ErrorMessage = "No white space allowed")]
-		public string RedStickerFileId { get; init;  }
-
-		[Required]
-		[RegularExpression(@"^\S*$", ErrorMessage = "No white space allowed")]
-		public string GreenStickerFileId { get; init;  }
-
+		
 		[Required]
 		[FormatStringPlaceholderIndexesCount(5)]
 		public string CurrencyRateMarkdownMessageTemplate { get; init; }
