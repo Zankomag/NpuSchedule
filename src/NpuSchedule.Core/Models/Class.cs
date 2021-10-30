9de147d6f94b5feb5b9460c51331a0427ef16f51
Adam Evans <Zankomag@gmail.com>
2021-10-31 01:41:11 +0300

    Improve field types

--- src/NpuSchedule.Core/Models/Class.cs
+++ src/NpuSchedule.Core/Models/Class.cs
@@ -1,11 +1,13 @@
+using System;
+
 namespace NpuSchedule.Core.Models
 {
     /// <summary>University class (the time when students get together to listen to a lesson of a particular subject in school)</summary>
     public class Class
     {
-        public int Number { get; internal set; }
-        public string StartTime { get; internal set; }
-        public string EndTime { get; internal set; }
+		public int Number { get; internal set; }
+        public TimeSpan StartTime { get; internal set; }
+        public TimeSpan EndTime { get; internal set; }
         public ClassInfo FirstClass { get; internal set; }
         public ClassInfo SecondClass { get; internal set; }
     }
