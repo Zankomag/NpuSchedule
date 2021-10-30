162dce4921c1be22ce59f4ae2b3797742703102a
Adam Evans <Zankomag@gmail.com>
2021-10-31 01:33:41 +0300

    Improve ScheduleDay fields

--- src/NpuSchedule.Core/Models/ScheduleDay.cs
+++ src/NpuSchedule.Core/Models/ScheduleDay.cs
@@ -1,11 +1,11 @@
+using System;
 using System.Collections.Generic;
 
 namespace NpuSchedule.Core.Models
 {
     public class ScheduleDay
     {
-        public string WeekName { get; internal set; }
-        public string Date { get; internal set; }
+		public DateTime Date { get; internal set; }
         public List<Class> Classes { get; internal set; }
     }
 }
