67b98b25cc653e6499d58c7be20a2a23b82685fe
Adam Evans <Zankomag@gmail.com>
2021-10-31 17:03:14 +0200

    Add windows-1251 encoding extensions

---
+++ src/NpuSchedule.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace NpuSchedule.Core.Extensions {
+
+	public static class StringExtensions {
+
+		private static Encoding windows1251Encoding;
+
+		private static Encoding Windows1251Encoding {
+			get {
+				if(windows1251Encoding != null) {
+					return windows1251Encoding;
+				}
+				Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+				windows1251Encoding = Encoding.GetEncoding(1251);
+				return windows1251Encoding;
+			}
+		}
+		
+		public static byte[] ToWindows1251(this string @string) => Windows1251Encoding.GetBytes(@string);
+
+		public static string FromWindows1251(this byte[] bytes) => Windows1251Encoding.GetString(bytes);
+
+	}
+
+}
