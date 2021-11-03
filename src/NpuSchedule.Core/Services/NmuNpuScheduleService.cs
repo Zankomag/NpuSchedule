e2458efa237a6e65a9309a7c8f706486358817cf
vladkryv <vladislavkrivovyaz@gmail.com>
2021-11-04 01:25:58 +0200

    Reffactoring, replace space to tab, error logging draft

--- src/NpuSchedule.Core/Services/NmuNpuScheduleService.cs
+++ src/NpuSchedule.Core/Services/NmuNpuScheduleService.cs
@@ -22,6 +22,7 @@ namespace NpuSchedule.Core.Services {
 
 		private readonly NpuScheduleOptions options;
 		private readonly ILogger<NmuNpuScheduleService> logger;
+		private const string TempDivider = "*|*";
 
 		public NmuNpuScheduleService(IOptions<NpuScheduleOptions> options, ILogger<NmuNpuScheduleService> logger) {
 			this.options = options.Value;
@@ -54,131 +55,139 @@ namespace NpuSchedule.Core.Services {
 			client.BaseAddress = new Uri(@"http://nmu.npu.edu.ua");
 			
 			//n=700 should be as url parameter, otherwise it doesn't work
-			var response = await client.PostAsync(@$"cgi-bin/timetable.cgi?n=700", new ByteArrayContent(contentBytes));
+			var response = await client.PostAsync(@"cgi-bin/timetable.cgi?n=700", new ByteArrayContent(contentBytes));
 
 			var responseContentBytes = await response.Content.ReadAsByteArrayAsync();
 			return responseContentBytes.FromWindows1251();
 		}
 		
 		static async Task<List<ScheduleDay>> ParseRangeSchedule(string rawHtml)
-        {
-            var result = new List<ScheduleDay>();
-            
-            var config = Configuration.Default.WithDefaultLoader();
-            var context = BrowsingContext.New(config);
-            var document = await context.OpenAsync(r => r.Content(rawHtml));
-            var daySelector = "div.container div.row div.col-md-6:not(.col-xs-12)";
-            var days = document.QuerySelectorAll(daySelector);
+		{
+			var result = new List<ScheduleDay>();
+			
+			var config = Configuration.Default.WithDefaultLoader();
+			var context = BrowsingContext.New(config);
+			var document = await context.OpenAsync(r => r.Content(rawHtml));
+			var daySelector = "div.container div.row div.col-md-6:not(.col-xs-12)";
+			var days = document.QuerySelectorAll(daySelector);
 
-            for (int i = 0; i < days.Length; i++)
-                result.Add(ParseDaySchedule(days[i]));
+			for (int i = 0; i < days.Length; i++)
+				result.Add(ParseDaySchedule(days[i]));
 
-            return result;
-        }
-        
-        static ScheduleDay ParseDaySchedule(IElement rawDay)
-        {
-            var rawDate = rawDay.QuerySelector("h4")?.TextContent.Split(" ")[0];
-            var date = DateTime.ParseExact(rawDate, "dd.MM.yyyy", CultureInfo.InvariantCulture);
-            
-            var classes = new List<Class>();
-            var rawClasses = rawDay.QuerySelectorAll("tr");
+			return result;
+		}
+		
+		static ScheduleDay ParseDaySchedule(IElement rawDay)
+		{
+			var rawDate = rawDay.QuerySelector("h4")?.TextContent.Split(" ")[0];
+			var date = DateTime.ParseExact(rawDate, "dd.MM.yyyy", CultureInfo.InvariantCulture);
+			//TODO add check error parse
+			
+			var classes = new List<Class>();
+			var rawClasses = rawDay.QuerySelectorAll("tr");
 
-            for (int i = 0; i < rawClasses.Length; i++)
-                if (rawClasses[i].InnerHtml.Contains("class=\"link\""))
-                    classes.Add(ParseClass(rawClasses[i]));
-            
-            return new ScheduleDay { Date = date, Classes = classes };
-        }
+			for (int i = 0; i < rawClasses.Length; i++)
+				if (rawClasses[i].InnerHtml.Contains("class=\"link\""))
+					classes.Add(ParseClass(rawClasses[i]));
+			
+			return new ScheduleDay { Date = date, Classes = classes };
+		}
 
-        static Class ParseClass(IElement rawClass)
-        {
-            ClassInfo firstClass = null;
-            ClassInfo secondClass = null;
-            int.TryParse(rawClass.QuerySelector("td:nth-child(1)")?.TextContent, out var numberClass);
-            string timeClass = rawClass.QuerySelector("td:nth-child(2)")?.TextContent.Insert(5, " ");
-            
-            var startTime = TimeSpan.Parse(timeClass.Split(" ")[0]);
-            var endTime = TimeSpan.Parse(timeClass.Split(" ")[1]);
-            
-            var countClassInfo = rawClass.InnerHtml.CountSubstring("class=\"link\"");
-            if (countClassInfo == 1)
-                firstClass = ParseClassInfo(rawClass.QuerySelector("td:nth-child(3)"));
-            else if (countClassInfo > 1)
-            {
-                var classInfo = rawClass.QuerySelector("td:nth-child(3)");
-                string tmp = classInfo?.InnerHtml;
+		static Class ParseClass(IElement rawClass)
+		{
+			ClassInfo firstClass = null;
+			ClassInfo secondClass = null;
+			
+			if(!int.TryParse(rawClass.QuerySelector("td:nth-child(1)")?.TextContent, out var numberClass))
+				Console.WriteLine("Error get or parse number class"); //TODO replace console write to logger
+			
+			const int indexOfEndTime = 5;
+			string timeClass = rawClass.QuerySelector("td:nth-child(2)")?.TextContent.Insert(indexOfEndTime, TempDivider);
+			var rawTimeStartAndEnd = timeClass?.Split(TempDivider);
+			var startTime = TimeSpan.Zero;
+			var endTime = TimeSpan.Zero;
+			
+			if(rawTimeStartAndEnd?.Length < 1 || !TimeSpan.TryParse(rawTimeStartAndEnd?[0], out startTime))
+				Console.WriteLine("Error get or parse start time"); //TODO replace console write to logger
+			
+			if(rawTimeStartAndEnd?.Length < 2 || !TimeSpan.TryParse(rawTimeStartAndEnd?[1], out endTime))
+				Console.WriteLine("Error get or parse end time"); //TODO replace console write to logger
 
-                if (classInfo != null)
-                {
-                    var startIndex = classInfo.InnerHtml.IndexOf("</div> ", StringComparison.Ordinal);
-                        
-                    // remove second
-                    if (startIndex != -1)
-                        classInfo.InnerHtml = classInfo.InnerHtml[..(startIndex + 7)]; // 7 = "</div> ".Length
-                    firstClass = ParseClassInfo(classInfo);
+			var countClassInfo = rawClass.InnerHtml.CountSubstring("class=\"link\"");
+			
+			if (countClassInfo == 1)
+				firstClass = ParseClassInfo(rawClass.QuerySelector("td:nth-child(3)"));
+			else if (countClassInfo > 1)
+			{
+				var classInfo = rawClass.QuerySelector("td:nth-child(3)");
+				
+				if (classInfo != null)
+				{
+					const string endFirstClass = "</div> ";
+					string tmp = classInfo.InnerHtml;
+					var startIndex = classInfo.InnerHtml.IndexOf(endFirstClass, StringComparison.Ordinal);
+						
+					// remove second
+					if (startIndex != -1)
+						classInfo.InnerHtml = classInfo.InnerHtml[..(startIndex + endFirstClass.Length)];
+					firstClass = ParseClassInfo(classInfo);
 
-                    // remove first and adaptation second classInfo for parser
-                    if (startIndex != -1)
-                        classInfo.InnerHtml = tmp[(startIndex + 11)..].Replace("  <div", "<br> <div"); // 11 = "</div> ".Length + "<br>".Length
-                    secondClass = ParseClassInfo(classInfo);
-                }
-            }
-            
-            return new Class { StartTime = startTime, EndTime = endTime, Number = numberClass ,FirstClass = firstClass, SecondClass = secondClass};
-        }
+					// remove first and adaptation second classInfo for parser
+					if (startIndex != -1)
+						classInfo.InnerHtml = tmp[(startIndex + "</div> <br>".Length)..].Replace("  <div", "<br> <div");
+					secondClass = ParseClassInfo(classInfo);
+				}
+			}
+			
+			return new Class { StartTime = startTime, EndTime = endTime, Number = numberClass ,FirstClass = firstClass, SecondClass = secondClass};
+		}
 
-        static ClassInfo ParseClassInfo(IElement classInfoObj)
-        {
-            string altDivider = "***";
-            var meetUrl = classInfoObj.QuerySelector("div.link a")?.InnerHtml;
-            classInfoObj.InnerHtml = classInfoObj.InnerHtml.Replace(" ауд", altDivider + "ауд");
+		static ClassInfo ParseClassInfo(IElement classInfoObj)
+		{
+			var meetUrl = classInfoObj.QuerySelector("div.link a")?.InnerHtml;
+			classInfoObj.InnerHtml = classInfoObj.InnerHtml.Replace(" ауд", TempDivider + "ауд");
 
-            bool isRemote = false;
-            if (classInfoObj.InnerHtml.Contains("class=\"remote_work\""))
-            {
-                classInfoObj.InnerHtml = classInfoObj.InnerHtml
-	                [(classInfoObj.InnerHtml.IndexOf("</span><br>", StringComparison.Ordinal) + 11)..]; // 11 = "</span><br>".Length
-                isRemote = true;
-            }
+			bool isRemote = false;
+			if (classInfoObj.InnerHtml.Contains("class=\"remote_work\""))
+			{
+				const string endSpanRemote = "</span><br>";
+				classInfoObj.InnerHtml = classInfoObj.InnerHtml
+					[(classInfoObj.InnerHtml.IndexOf(endSpanRemote, StringComparison.Ordinal) + endSpanRemote.Length)..];
+				isRemote = true;
+			}
 
-            var discipline = classInfoObj.ChildNodes[0].TextContent.Trim();
-            string classroom = null;
-            string teacher = null;
+			var discipline = classInfoObj.ChildNodes[0].TextContent.Trim();
+			string classroom = null;
+			string teacher = null;
 
-            switch (classInfoObj.ChildNodes.Length)
-            {
-                case 9:
-                    
-                    if (classInfoObj.ChildNodes[4].TextContent.Contains(altDivider))
-                    {
-                        teacher = classInfoObj.ChildNodes[4].TextContent.Trim().Split(altDivider)[0];
-                        classroom = classInfoObj.ChildNodes[4].TextContent.Trim().Split(altDivider)[1];
-                    }
-                    else
-                        teacher = classInfoObj.ChildNodes[4].TextContent.Trim();
-                    
-                    break;
-                case 7:
-                case 5:
-                    
-                    if (classInfoObj.ChildNodes[2].TextContent.Contains(altDivider))
-                    {
-                        teacher = classInfoObj.ChildNodes[2].TextContent.Trim().Split(altDivider)[0];
-                        classroom = classInfoObj.ChildNodes[2].TextContent.Trim().Split(altDivider)[1];
-                    }
-                    else
-                        teacher = classInfoObj.ChildNodes[2].TextContent.Trim();
-                    
-                    break;
-            }
+			var childIndex = classInfoObj.ChildNodes.Length switch
+			{
+				9 => // two string is data mixed group
+					4,
+				7 or 5 => // two string is teacher and maybe has classroom
+					2,
+				_ => -1
+			};
 
-            return new ClassInfo {
-                DisciplineName = discipline, Teacher = teacher, Classroom = classroom,
-                OnlineMeetingUrl = meetUrl, IsRemote = isRemote
-            };
-        }
-        
+			if (childIndex != -1)
+			{
+				var rawTeacherAndClassroom = classInfoObj.ChildNodes[childIndex].TextContent;
+				if (rawTeacherAndClassroom.Contains(TempDivider))
+				{
+					var teacherAndClassroom = rawTeacherAndClassroom.Split(TempDivider);
+					teacher = teacherAndClassroom[0].Trim();
+					classroom = teacherAndClassroom[1].Trim();
+				}
+				else
+					teacher = rawTeacherAndClassroom.Trim();
+			}
+			
+			return new ClassInfo {
+				DisciplineName = discipline, Teacher = teacher, Classroom = classroom,
+				OnlineMeetingUrl = meetUrl, IsRemote = isRemote
+			};
+		}
+		
 	}
 
 }
