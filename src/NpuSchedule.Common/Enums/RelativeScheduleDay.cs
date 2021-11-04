c02d9d02d06913e5e882b2e1477da9b1564470a2
Adam Evans <Zankomag@gmail.com>
2021-11-04 21:53:58 +0200

    Add Relative schedule day extensions

--- src/NpuSchedule.Common/Enums/RelativeScheduleDay.cs
+++ src/NpuSchedule.Common/Enums/RelativeScheduleDay.cs
@@ -1,9 +1,15 @@
-namespace NpuSchedule.Common.Enums
-{
-    public enum RelativeScheduleDay
-    {
-        Today = 0,
-        Tomorrow = 1,
-        Closest = 2
-    }
+namespace NpuSchedule.Common.Enums {
+
+	public enum RelativeScheduleDay {
+
+		Today = 0,
+		Tomorrow = 1,
+
+		/// <summary>
+		///     The closes day that contains a schedule
+		/// </summary>
+		Closest = 2
+
+	}
+
 }
