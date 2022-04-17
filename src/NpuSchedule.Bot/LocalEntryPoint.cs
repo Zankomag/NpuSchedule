59670edb833038e28a9f2b8297d9ee44a5b9cc7c
Adam Evans <zankomag@gmail.com>
2022-04-17 14:40:37 +0300

    Update to file-scoped namespace

--- src/NpuSchedule.Bot/LocalEntryPoint.cs
+++ src/NpuSchedule.Bot/LocalEntryPoint.cs
@@ -6,36 +6,34 @@ using NpuSchedule.Bot.Services;
 using NpuSchedule.Common.Extensions;
 using NpuSchedule.Common.Utils;
 
-namespace NpuSchedule.Bot {
+namespace NpuSchedule.Bot; 
 
-	public class LocalEntryPoint {
+public class LocalEntryPoint {
 
-		private static async Task Main() {
-			IHost host;
-			if(EnvironmentWrapper.IsDevelopment) {
-				host = GetHost();
-			} else {
-				host = GetWebHost();
-			}
-
-			await host.RunAsync();
+	private static async Task Main() {
+		IHost host;
+		if(EnvironmentWrapper.IsDevelopment) {
+			host = GetHost();
+		} else {
+			host = GetWebHost();
 		}
 
-		private static IHost GetHost()
-			=> new HostBuilder()
-				.AddConfiguration()
-				.UseStartup<Startup>()
-				.ConfigureServices(services => services.AddHostedService<TelegramBotLocalRunner>())
-				.Build();
-
-		private static IHost GetWebHost()
-			=> new HostBuilder()
-				.AddConfiguration()
-				.ConfigureWebHost(x =>
-					x.UseKestrel((builderContext, options) => options.Configure(builderContext.Configuration.GetSection("Kestrel")))
-						.UseStartup<Startup>())
-				.Build();
-
+		await host.RunAsync();
 	}
+
+	private static IHost GetHost()
+		=> new HostBuilder()
+			.AddConfiguration()
+			.UseStartup<Startup>()
+			.ConfigureServices(services => services.AddHostedService<TelegramBotLocalRunner>())
+			.Build();
+
+	private static IHost GetWebHost()
+		=> new HostBuilder()
+			.AddConfiguration()
+			.ConfigureWebHost(x =>
+				x.UseKestrel((builderContext, options) => options.Configure(builderContext.Configuration.GetSection("Kestrel")))
+					.UseStartup<Startup>())
+			.Build();
 
 }
